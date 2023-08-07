043d01f750e4c8d197507b83cd52153f50a3066d
FloreauLuca <lucafloreau@akeonet.com>
2023-08-08 00:09:55 +0200

    Start about and projects layout

--- portfolio/js/title.js
+++ portfolio/js/title.js
@@ -7,7 +7,7 @@ carousel();
 
 function carousel() {
     var i;
-    var x = document.getElementsByClassName("mySlides");
+    var x = document.getElementsByClassName("titleImg");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
